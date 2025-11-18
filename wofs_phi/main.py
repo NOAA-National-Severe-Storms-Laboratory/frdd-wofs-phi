54df67444dbf7ce1b4337aa47ef3d1ebc19723e2
eloken-wx <eloken@ou.edu>
2025-11-18 14:18:03 -0600

    initial commit for refactor

---
+++ wofs_phi/main.py
@@ -0,0 +1,25 @@
+#==================================================
+# This script will function as the main method for 
+# running WoFS-PHI (v2; after refactoring)
+#
+# Started by: Eric Loken 11/18/2025
+#==================================================
+
+import numpy as np 
+
+
+
+
+def main(): 
+
+
+    return 
+
+
+
+
+if (__name__ == '__main__'): 
+
+    main() 
+
+
--- wofs_phi/test.py
+++
@@ -1,7 +0,0 @@
-import os 
-import pathlib
-
-base_path = pathlib.Path(__file__).parent.resolve()
-
-print (base_path) 
-
